8b2dc146c3a8666955dcca144730bb2cc7a5dd16
permlap-phola <permlap@tatugacamp.com>
2024-02-26 09:43:11 +0700

    Fix Syxtax Sitemap (https://github.com/tatuga-camp/client-main-koratkos/pull/1)

--- pages/sitemap.xml.ts
+++ pages/sitemap.xml.ts
@@ -1,14 +1,14 @@
 import { GetServerSideProps, GetServerSidePropsContext } from "next";
 
 function generateSiteMap() {
-  return `  <xml version="1.0" encoding="UTF-8">
-                <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
-                    <url>
-                        <loc>https://koratkos.org</loc>
-                        <lastmod>2024-02-26</lastmod>
-                    </url>
-                </urlset>
-            </xml>
+  return `<?xml version="1.0" encoding="UTF-8"?>
+  <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
+    <!--We manually set the two URLs we know already-->
+    <url>
+    <loc>https://koratkos.org</loc>
+<lastmod>2024-02-26</lastmod>
+    </url>
+  </urlset>
  `;
 }
 
